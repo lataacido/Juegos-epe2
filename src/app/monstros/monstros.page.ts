ea8bfb3338cdf77598b0a5ea2f26b33e6d2a9f55
Beginning0 <39624026+Beginning0@users.noreply.github.com>
2019-05-24 10:00:04 -0400

    Update monstros.page.ts
    TODO FULL

--- src/app/monstros/monstros.page.ts
+++ src/app/monstros/monstros.page.ts
@@ -15,6 +15,81 @@ export class MonstrosPage implements OnInit {
   ngOnInit() {
   }
 
+  Comprar1 : boolean;
+  Comprar2 : boolean;
+  Comprar3 : boolean;
+  Comprar4 : boolean;
+
+  async verComprar1(){
+
+    if(this.Comprar1){
+
+    }else{
+      
+      let alert = await this.alertCrtl.create({
+        header: 'Anjanath - Monster Hunter World',
+        subHeader : 'El Anjanath patrulla el Bosque Antiguo en busca de su comida favorita, Aptonoth. Este monstruo beligerante atacará cualquier cosa sin la menor provocación.',
+        message:'<ion-img src="/assets/anjanath.jpg"></ion-img>',
+        buttons:['Cerrar']
+      });
+      alert.present();
+    
+    }
+
+  }
+  
+  async verComprar2(){
+
+    if(this.Comprar2){
+
+    }else{
+      
+      let alert = await this.alertCrtl.create({
+        header: 'Nergigante - Monster Hunter World',
+        subHeader : 'Un terrible dragón anciano que aparece cuando otros ancianos están cerca. Su inclinación por la destrucción está bien documentada',
+        message:'<ion-img src="/assets/nergigante.jpg"></ion-img>',
+        buttons:['Cerrar']
+      });
+      alert.present();
+    
+    }
+
+  }
+  
+  async verComprar3(){
+
+    if(this.Comprar3){
+
+    }else{
+      
+      let alert = await this.alertCrtl.create({
+        header: 'Diablos - Monster Hunter World',
+        subHeader : 'El Diablos es un Wyvern Volador que domina zonas desérticas, fue introducido en Monster Hunter. ',
+        message:'<ion-img src="/assets/Diablos.jpg"></ion-img>',
+        buttons:['Cerrar']
+      });
+      alert.present();
+    
+    }
+
+  }
+  async verComprar4(){
+
+    if(this.Comprar4){
+
+    }else{
+      
+      let alert = await this.alertCrtl.create({
+        header: 'Diablos - Monster Hunter World',
+        subHeader : 'El Diablos es un Wyvern Volador que domina zonas desérticas, fue introducido en Monster Hunter. ',
+        message:'<ion-img src="/assets/Diablos.jpg"></ion-img>',
+        buttons:['Cerrar']
+      });
+      alert.present();
+    
+    }
+
+  }
 
   paginas(){
     this.navController.navigateForward('paginas')
