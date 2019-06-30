da940f95d731ae7cf4a542a9e50ee2b19b73c9e1
Beginning0 <39624026+Beginning0@users.noreply.github.com>
2019-06-30 09:25:03 -0400

    terro
    ya modifique como se deve

--- src/app/monstros/monstros.page.ts
+++ src/app/monstros/monstros.page.ts
@@ -13,9 +13,9 @@ import { CrudService } from '../conexfirebase/conexfirebase.page';
 export class MonstrosPage implements OnInit {
   
   students: any;
-  studentName: string;
-  studentAge: number;
-  studentAddress: string;
+  juegosNombre: string;
+  juegosPrecio: number;
+  juegosPlata: string;
 
 
   constructor(private crudService: CrudService, public navController : NavController, public alertCrtl : AlertController) { }
@@ -28,52 +28,15 @@ export class MonstrosPage implements OnInit {
         return {
           id: e.payload.doc.id,
           isEdit: false,
-          Name: e.payload.doc.data()['Name'],
-          Age: e.payload.doc.data()['Age'],
-          Address: e.payload.doc.data()['Address'],
+          Nombre: e.payload.doc.data()['Nombre'],
+          Precio: e.payload.doc.data()['Precio'],
+          Plata: e.payload.doc.data()['Plata'],
         };
       })
       console.log(this.students);
 
     });
   }
-
-  CreateRecord() {
-    let record = {};
-    record['Name'] = this.studentName;
-    record['Age'] = this.studentAge;
-    record['Address'] = this.studentAddress;
-    this.crudService.create_NewStudent(record).then(resp => {
-      this.studentName = "";
-      this.studentAge = undefined;
-      this.studentAddress = "";
-      console.log(resp);
-    })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
-  RemoveRecord(rowID) {
-    this.crudService.delete_Student(rowID);
-  }
-
-  EditRecord(record) {
-    record.isEdit = true;
-    record.EditName = record.Name;
-    record.EditAge = record.Age;
-    record.EditAddress = record.Address;
-  }
-
-  UpdateRecord(recordRow) {
-    let record = {};
-    record['Name'] = recordRow.EditName;
-    record['Age'] = recordRow.EditAge;
-    record['Address'] = recordRow.EditAddress;
-    this.crudService.update_Student(recordRow.id, record);
-    recordRow.isEdit = false;
-  }
-
   paginas(){
     this.navController.navigateForward('paginas')
   }
