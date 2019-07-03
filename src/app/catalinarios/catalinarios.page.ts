0d29a54f3a2f19d42d82e81c412d42e69be09e35
catalinarioslange <cata.rios.la@gmail.com>
2019-07-02 22:35:46 -0400

    subiendo algo

--- src/app/catalinarios/catalinarios.page.ts
+++ src/app/catalinarios/catalinarios.page.ts
@@ -1,16 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import {NavController} from '@ionic/angular';
 import { AlertController } from '@ionic/angular';
+
+import { CrudService } from '../conexfirebase/conexfirebase.page';
 @Component({
   selector: 'app-catalinarios',
   templateUrl: './catalinarios.page.html',
   styleUrls: ['./catalinarios.page.scss'],
 })
 export class CatalinariosPage implements OnInit {
+  
+
+  students: any;
+  juegosNombre: string;
+  juegosPrecio: number;
+  juegosPlata: string;
+
+
  perso : string;
  public boton:boolean=false;
-  constructor(public navController : NavController, public alertCtrl: AlertController ) { }
+  constructor(private crudService: CrudService, public navController : NavController, public alertCrtl : AlertController) { }
   ngOnInit() {
+    this.crudService.read_Students().subscribe(data => {
+
+      this.students = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          isEdit: false,
+          Nombre: e.payload.doc.data()['Nombre'],
+          Precio: e.payload.doc.data()['Precio'],
+          Plata: e.payload.doc.data()['Plata'],
+          URL:e.payload.doc.data()['URL'],
+        };
+      })
+      console.log(this.students);
+
+    });
   }
   paginas(){
     this.navController.navigateForward('paginas')
@@ -24,7 +49,7 @@ export class CatalinariosPage implements OnInit {
     if(this.boton){
 
     }else{
-    let alert = await this.alertCtrl.create({
+    let alert = await this.alertCrtl.create({
       header: this.perso,
       message:"Personaje Principal de StarCraft",      
       buttons:['OK']
